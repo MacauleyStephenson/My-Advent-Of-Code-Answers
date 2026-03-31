12d952e5569c33bebf5d2c1229d335949b541227
MacauleyStephenson <MacauleyStephenson@mail.com>
2026-03-31 19:50:23 +0300

    Part 2 of Day 1 solved, added extra statements for edge caes

--- 2025/Day 1 Part 2/Day1.py
+++ 2025/Day 1 Part 2/Day1.py
@@ -4,14 +4,27 @@ with open(sys.argv[1], 'r') as f:
     lines = list(map(str.strip, f.readlines()))
 
 part1 = 0
+part2 = 0
 pos = 50
 
 for line in lines:
     val = int(line.replace('L', '-').replace('R', ''))
-    pos = (pos + val) % 100
+    #pos = 0, L5
+    if pos == 0 and val < 0:
+        part2 -= 1 
+    pos = pos + val
+
+    part2 += abs(pos // 100)
+    #Handles pos = 5, L5
+    if pos == 0:
+        part2 += 1
+    #Handles pos = 5, L 105
+
+    if pos % 100 == 0 and pos < 0:
+        part2 += 1
+    pos = pos % 100
     if pos == 0:
         part1 += 1
 print(f'Part 1: {part1}')
 
-part2 = ""
 print(f'Part 2: {part2}')
