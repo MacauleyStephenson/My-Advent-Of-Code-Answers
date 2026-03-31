8e5a29fe1f302cf9cf6b8f96ae3b00d627d57ecd
MacauleyStephenson <MacauleyStephenson@mail.com>
2026-03-31 22:01:11 +0300

    Day 2 part 2 completed

--- 2025/Day 2 part 2/part2.py
+++ 2025/Day 2 part 2/part2.py
@@ -11,17 +11,21 @@ def is_invalid(i: int) -> bool:
 def is_invalid_regex(i: int) -> bool:
     return bool(re.fullmatch(r'(.+)\1', str(i)))
 
+def is_invalid_regex_part2(i: int) -> bool:
+    return bool(re.fullmatch(r'(.+)\1+', str(i)))
+
 with open(sys.argv[1], 'r') as f: 
     ranges = f.read().strip().split(',')
 
 part1 = 0
+part2 = 0
 for r in ranges:
     start, end = map(int, r.split('-'))
     for i in range(start, end +1):
         if is_invalid(i):
             part1 += i
+        if is_invalid_regex_part2(i):
+            part2 += i
 
 print(f'Part 1: {part1}')
-
-part2 = ""
 print(f'Part 2: {part2}')
