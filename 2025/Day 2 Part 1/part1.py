0892b43a314c3c8b410db9bfd2e0eec50dd71141
MacauleyStephenson <MacauleyStephenson@mail.com>
2026-03-31 21:25:46 +0300

    Day 2 part 1 completed

--- 2025/Day 2 Part 1/part1.py
+++ 2025/Day 2 Part 1/part1.py
@@ -1,11 +1,23 @@
 import sys
 
+def is_invalid(i: int) -> bool:
+    code = str(i)
+    if len(code) % 2 != 0:
+        return False
+    return code[:len(code)//2] == code[len(code) //2:]
 
-with open(sys.argv[1], 'r') as f:
+
+with open(sys.argv[1], 'r') as f: 
+    ranges = f.read().strip().split(',')
 
 
-part1: ""
-print(f'Part 1: {Part1}')
+part1 = 0
+for r in ranges:
+    start, end = map(int, r.split('-'))
+    for i in range(start, end +1):
+        if is_invalid(i):
+            part1 += i
+print(f'Part 1: {part1}')
 
-part2: ""
+part2 = ""
 print(f'Part 2: {part2}')
