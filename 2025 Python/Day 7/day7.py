b5cf04728cfaf9a73c81d9038711d231c90a87c0
MacauleyStephenson <MacauleyStephenson@mail.com>
2026-04-04 22:46:41 +0300

    Day 7 part 1 complete

--- 2025 Python/Day 7/day7.py
+++ 2025 Python/Day 7/day7.py
@@ -4,7 +4,20 @@ import sys
 with open(sys.argv[1], 'r') as f:
     lines = list(map(str.strip, f.readlines()))
 
-part1 = ""
+beams = [lines[0].index('S')]
+part1 = 0 
+
+for line in lines[1:]:
+    next_beams = set()
+    for beam in beams:
+        if line[beam] == ".":
+            next_beams.add(beam)
+        elif line[beam] == "^":
+            next_beams.add(beam - 1)
+            next_beams.add(beam + 1)
+            part1 += 1
+    beams = next_beams
+
 print(f"Part 1: {part1}")
 
 part2 = ""
