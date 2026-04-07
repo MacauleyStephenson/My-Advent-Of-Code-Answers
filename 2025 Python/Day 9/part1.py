e92dc1bd173adaf1c3fe366d757763dc98f09a57
MacauleyStephenson <MacauleyStephenson@mail.com>
2026-04-07 23:49:08 +0300

    Day 9 part 1 complete

--- 2025 Python/Day 9/part1.py
+++ 2025 Python/Day 9/part1.py
@@ -3,7 +3,18 @@ import sys
 with open(sys.argv[1], 'r') as f:
     lines = list(map(str.strip, f.readlines()))
 
-part1 = ""
+points = []
+for line in lines:
+    x, y = map(int, line.split(','))
+    points.append((x, y))
+
+part1 = 0
+for i, (x1, y1) in enumerate(points):
+    for j, (x2, y2) in enumerate(points):
+        if i < j:
+            area = (abs(x1 - x2) + 1) * (abs(y1 - y2) +1)
+            part1 = max(part1, area)
+            
 print(f"Part 1: {part1}")
 
 part2 = ""
