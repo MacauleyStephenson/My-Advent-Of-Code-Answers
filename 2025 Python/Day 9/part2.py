e20ebb57cb2c800330c8254530c3fc240da40e01
MacauleyStephenson <MacauleyStephenson@mail.com>
2026-04-08 15:13:43 +0300

    part 2 fixing edge cases

--- 2025 Python/Day 9/part2.py
+++ 2025 Python/Day 9/part2.py
@@ -1,26 +1,39 @@
 import sys
+from functools import cache
 
+@cache
 def point_in_poly(x: int, y: int) -> bool:
     inside = False
 
-    for (x1, y1,), (x2, y2) in zip(points, points[1:] + points[:1]):
+    for (x1, y1), (x2, y2) in zip(points, points[1:] + points[:1]):
         if (x == x1 == x2 and min(y1, y2) <= y <= max(y1, y2) or
             y == y1 == y2 and min(x1, x2) <= x <= max(x1, x2)):
             return True
-        #assert (x1 == x2 and y1 != y2) or (x1 != x2 and y1 == y2)
-        if ((y1 > y) != (y2 > y)) and (x < (x2 - x1) * (y - y1) / (y2 - y1) +x1):
+        if ((y1 > y) != (y2 > y)) and (x < (x2 - x1) * (y - y1) / (y2 - y1) + x1):
             inside = not inside
         
         return inside
-    
+
+def edge_intersects_rect(x1, y1, x2, y2, rx1, ry1, rx2, ry2):
+    if y1 == y2:
+        if max(x1, x2) > rx1 and min(x1, x2) < rx2:
+            return True
+    else:
+        if rx1 < x1 < rx2:
+            if max(y1, y2) > ry1 and min(y1, y2) < ry2:
+                return True
+    return False
+
 def square_valid(x1, x2, y1, y2):
     x1, x2 = sorted([x1, x2])
     y1, y2 = sorted([y1, y2])
     
-    for x in range(x1, x2 + 1):
-        for y in range(y1, y2 + 1):
-            if not point_in_poly(x, y):
-                return False
+    for x, y in [(x1, y1), (x1, y2), (x2, y1), (x2, y2)]:
+        if not point_in_poly(x, y):
+            return False
+    for (ex1, ey1), (ex2, ey2) in zip(points, points[1:] + points[:1]):
+        if edge_intersects_rect(ex1, ey1, ex2, ey2, x1, y1, x2, y2):
+            return False
     
     return True
 
