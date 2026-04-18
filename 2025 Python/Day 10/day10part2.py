fb9c12de101f52f0a7d0a6a6eabff054f4a5c49e
MacauleyStephenson <MacauleyStephenson@mail.com>
2026-04-18 19:40:34 +0300

    Day 10 reparsing lines

---
+++ 2025 Python/Day 10/day10part2.py
@@ -0,0 +1,36 @@
+import sys
+from itertools import combinations
+#from z3 import Optimize
+
+def find_min_pushes(target, buttons):
+    for i in range(1, len(buttons) + 1):
+        for comb in combinations(buttons, i):
+            result = 0
+            for c in comb:
+                result ^= c
+            if result == target:
+                return i
+
+def find_min_pushes2(target, buttons):
+    return 0 
+
+with open(sys.argv[1], 'r') as f:
+    lines = list(map(str.strip, f.readlines()))
+
+part1 = 0
+part2 = 0
+for line in lines:
+    target_s, *buttons_s, power_s = line.split()
+    target = int(target_s.strip('[]')[::-1].replace('#', '1').replace('.', '0'), 2)    
+    buttons = [sum(pow(2, int(b)) for b in button.strip('()').split(',')) for button in buttons_s]
+
+    part1 += find_min_pushes(target, buttons)
+
+    buttons2 = [list(map(int, b.strip('()').split(','))) for b in buttons_s]
+    target2 = list(map(int, power_s.strip('{}').split(',')))
+    print(buttons2)
+    part2 += find_min_pushes2(target2, buttons)
+print(f"Part 1: {part1}")
+
+
+print(f"Part 2: {part2}")
