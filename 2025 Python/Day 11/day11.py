9494a02544fcacc0542f9fa118ff81010bba5b22
MacauleyStephenson <MacauleyStephenson@mail.com>
2026-04-19 22:34:45 +0300

    Day 11 part 1 complete

--- 2025 Python/Day 11/day11.py
+++ 2025 Python/Day 11/day11.py
@@ -1,9 +1,21 @@
 import sys
 
+
+def count_paths(start: str, end: str) -> int:
+      if start == end:
+            return 1
+    
+      return sum(count_paths(n, end) for n in node_map[start])
+
 with open(sys.argv[1], 'r') as f:
     lines = list(map(str.strip, f.readlines()))
 
-part1 = ""
+node_map = {}
+for line in lines:
+        node, *outs = line.split()
+        node_map[node.strip(':')] = outs
+
+part1 = count_paths('you', 'out')
 print(f"Part 1: {part1}")
 
 part2 = ""
